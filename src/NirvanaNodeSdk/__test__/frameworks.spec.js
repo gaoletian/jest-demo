faf7db971260d530a1cd55ac429c383161082ce8
gaoletian <gaoletian@finupgroup.com>
2017-04-01 03:37:55 +0800

    update test

--- src/NirvanaNodeSdk/__test__/frameworks.spec.js
+++ src/NirvanaNodeSdk/__test__/frameworks.spec.js
@@ -1,30 +1,21 @@
-import {Message, ReplyResult} from '../../common'
-import Model from '../../kernel/model'
-import * as Provider from '../../provider'
-import CoreService from '../../kernel/service'
+import {Message, ReplyResult} from '../common'
+import Model from '../kernel/model'
+import * as Provider from '../provider'
+import CoreService from '../kernel/service'
 
 
 /**
  * 静态导入测试
  */
 describe('es6 import test', () => {
-  it('import provider', () => {
-    expect(Provider).toBeDefined()
-  });
-  it('import service', () => {
-    expect(CoreService).toBeDefined()
-  });
 
-  it('import model', () => {
-    expect(Model).toBeDefined()
-  });
-  it('import Message', () => {
-    expect(Message).toBeDefined()
-  });
-  it('import ReplyResult', () => {
-    expect(ReplyResult).toBeDefined()
-  });
+  let allImport = {Provider, Message, ReplyResult, CoreService}
 
+  Object.keys(allImport).forEach(item => {
+    it(`import ${item}`, () => {
+      expect(allImport[item]).toBeDefined()
+    });
+  })
 
 });
 
@@ -35,11 +26,11 @@ describe('name-space test', () => {
   it('provider.nirvana', () => {
     expect(Provider.nirvana).toBeDefined()
     expect(Provider.nirvana.user).toBeDefined()
-    // expect(provider.nirvana.user.addUser).toBeDefined()
+    expect(Provider.nirvana.user.addUser).toBeDefined()
   });
   it('provider.jiea', () => {
     expect(Provider.jiea).toBeDefined()
-    // expect(provider.jiea.addUser).toBeDefined()
+    expect(Provider.jiea.addUser).toBeDefined()
   });
 });
 
