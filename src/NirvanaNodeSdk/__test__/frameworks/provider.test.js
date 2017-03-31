fb230c82a619c43a4a8436b63e7c3299e8f4f7a6
gaoletian <gaoletian@finupgroup.com>
2017-04-01 03:22:43 +0800

    add kernel/service and update test

--- src/NirvanaNodeSdk/__test__/frameworks/provider.test.js
+++ src/NirvanaNodeSdk/__test__/frameworks/provider.test.js
@@ -1,19 +1,30 @@
-import model from '../../kernel/model'
-import provider from '../../provider'
+import {Message, ReplyResult} from '../../common'
+import Model from '../../kernel/model'
+import * as Provider from '../../provider'
+import CoreService from '../../kernel/service'
 
-import Model from 'NirvanaNodeSdk/kernel/model'
-import * as Provider from 'NirvanaNodeSdk/provider'
 
 /**
  * 静态导入测试
  */
 describe('es6 import test', () => {
   it('import provider', () => {
-    expect(provider).toBeDefined()
+    expect(Provider).toBeDefined()
   });
+  it('import service', () => {
+    expect(CoreService).toBeDefined()
+  });
+
   it('import model', () => {
-    expect(model).toBeDefined()
+    expect(Model).toBeDefined()
   });
+  it('import Message', () => {
+    expect(Message).toBeDefined()
+  });
+  it('import ReplyResult', () => {
+    expect(ReplyResult).toBeDefined()
+  });
+
 
 });
 
@@ -22,12 +33,12 @@ describe('es6 import test', () => {
  */
 describe('name-space test', () => {
   it('provider.nirvana', () => {
-    expect(provider.nirvana).toBeDefined()
-    expect(provider.nirvana.user).toBeDefined()
+    expect(Provider.nirvana).toBeDefined()
+    expect(Provider.nirvana.user).toBeDefined()
     // expect(provider.nirvana.user.addUser).toBeDefined()
   });
   it('provider.jiea', () => {
-    expect(provider.jiea).toBeDefined()
+    expect(Provider.jiea).toBeDefined()
     // expect(provider.jiea.addUser).toBeDefined()
   });
 });
@@ -37,24 +48,24 @@ describe('name-space test', () => {
  * 返回结果测试
  */
 describe(`"provider.nirvana.user" all method should contain { code, message, result}`, async () => {
-  const allMethod = Object.keys(provider.nirvana.user)
+  const allMethod = Object.keys(Provider.nirvana.user)
   const expected = ['code', 'message', 'result']
 
   allMethod.forEach( fucName => {
     it(`provider.nirvana.user.${fucName}()`, async () => {
-      let res = await provider.nirvana.user[fucName]()
+      let res = await Provider.nirvana.user[fucName]()
       expect(Object.keys(res)).toEqual(expect.arrayContaining(expected));
     })
   })
 })
 
 describe(`"provider.jiea" all method should contain { code, message, result}`, async () => {
-  const allMethod = Object.keys(provider.jiea)
+  const allMethod = Object.keys(Provider.jiea)
   const expected = ['code', 'message', 'result']
 
   allMethod.forEach( fucName => {
     it(`provider.jiea.${fucName}()`, async () => {
-      let res = await provider.jiea[fucName]()
+      let res = await Provider.jiea[fucName]()
       expect(Object.keys(res)).toEqual(expect.arrayContaining(expected));
     })
   })
--- src/NirvanaNodeSdk/provider/nirvana/user/index.js
+++ src/NirvanaNodeSdk/provider/nirvana/user/index.js
@@ -1,3 +1,4 @@
 export * from './addUser'
 export * from './isUserExistByMobile'
 export * from './isUserExistByIdCard'
+export * from './modifyUser'
